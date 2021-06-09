de91699c7d088182fd1c208e64e9da3d50a65cd4
Alexandr <alexandr.capovsky@siemens.com>
2021-06-09 21:07:12 +0200

    Check if the number is valid in the grid

--- src/main/java/game/SudokuGame.java
+++ src/main/java/game/SudokuGame.java
@@ -11,17 +11,35 @@ public class SudokuGame {
 
     /**
      * Method copy defined board to new board and check if the board is valid
-     * @param board
+     * @param boardFromUser
      * @return
      */
-    private boolean copyDefinedGridToTheBoard(int[][]board) {
+    private boolean copyDefinedGridToTheBoard(int[][]boardFromUser) {
         for (int i = 0; i < DIMENSION; i++) {
             for (int j = 0; j < DIMENSION; j++) {
-                this.board[i][j] = board[i][j];
+                int number=boardFromUser[i][j];
+                isNumberValid(number,i,j);
+                this.board[i][j] = number;
             }
 
         }
         return true;
+    }
+    private boolean isNumberSingleDigit(int number, int row, int column){
+        if (number<=9 &&number>=0){
+            return true;
+        }
+        else {
+            throw new RuntimeException("number in row"+row+ "and column"+column+" is not single digit");
+
+        }
+
+    }
+    private boolean isNumberInRow(int number){
+        return false;
+    }
+    private boolean isNumberValid(int number, int row, int column){
+        return isNumberSingleDigit(number,row,column);
     }
 
 }
